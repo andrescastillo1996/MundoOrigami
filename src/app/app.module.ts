0a981f7d8f331d6f4c4b2c014db19fbc7c4cd20e
Andres Castillo <castillo.andres1300@gmail.com>
2024-09-01 14:16:22 -0500

    Se agrega modulos y se agrega la conexión con firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"mundoorigami-c963e","appId":"1:359659078282:web:694b29a419b0922661a373","storageBucket":"mundoorigami-c963e.appspot.com","apiKey":"AIzaSyDCcqe0rTmYSsZ2LPbBYdJbcZNSHe9bGZI","authDomain":"mundoorigami-c963e.firebaseapp.com","messagingSenderId":"359659078282","measurementId":"G-4YF297E3YK"})), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
