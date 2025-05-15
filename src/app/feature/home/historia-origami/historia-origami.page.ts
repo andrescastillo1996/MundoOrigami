a6fc8f15f0f0ac0fa84d45b8f7974e5ef335b1d8
Andres Castillo <aj.faru@gmail.com>
2025-05-14 20:18:11 -0500

    TASK0007: Se realiza la parte de la historia origami con los ejemplos practicos siendo consumidos desde firebase, centrado y visual, el boton de volver para regresar al home

--- src/app/feature/home/historia-origami/historia-origami.page.ts
+++ src/app/feature/home/historia-origami/historia-origami.page.ts
@@ -1,25 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import {
   IonContent,
   IonHeader,
   IonTitle,
   IonToolbar,
-} from '@ionic/angular/standalone';
+  IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-historia-origami',
   templateUrl: './historia-origami.page.html',
   styleUrls: ['./historia-origami.page.scss'],
   standalone: true,
-  imports: [
-    IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar,
-    CommonModule,
-    FormsModule,
+  imports: [IonIcon, IonButton,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonButtons,
+  IonIcon,
+  CommonModule,
+  FormsModule,
+  RouterModule,
   ],
 })
-export class HistoriaOrigamiPage {}
+export class HistoriaOrigamiPage {
+  private firestore: Firestore = inject(Firestore);
+  ejemplos_practicos = signal<any[]>([]);
+
+  constructor() {
+    const ejemplosRef = collection(this.firestore, 'ejemplos_practicos');
+    collectionData(ejemplosRef, { idField: 'id' }).subscribe((data) => {
+      this.ejemplos_practicos.set(data);
+    });
+  }
+}
